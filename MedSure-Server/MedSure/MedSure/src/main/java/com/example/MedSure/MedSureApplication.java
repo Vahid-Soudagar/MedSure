8972daf112951138889c4a571afb876484beae3e
Vahid-Soudagar <mohammadvahidsoudagar264@gmail.com>
2023-12-22 22:00:18 +0530

    on going server

---
+++ MedSure-Server/MedSure/MedSure/src/main/java/com/example/MedSure/MedSureApplication.java
@@ -0,0 +1,13 @@
+package com.example.MedSure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedSureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedSureApplication.class, args);
+	}
+
+}
---
+++ MedSure-Server/MedSure/MedSure/src/test/java/com/example/MedSure/MedSureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MedSure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedSureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
